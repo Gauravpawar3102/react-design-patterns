4953a5cbb560ac6502ae394acdd3e1759bf744f1
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-12-15 22:31:33 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { SplitScreen } from './SplitScreen';
+
+const LeftHandComponent = () => {
+  return <h1>Left</h1>;
+};
+
+const RightHandComponent = () => {
+  return <h1>Right</h1>;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <SplitScreen left={LeftHandComponent} right={RightHandComponent} />;
 }
 
 export default App;
